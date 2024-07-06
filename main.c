313bddcd4ae438ab40b1ad0380c929f6a488b9af
Xavier Moises Alberto Jose <xjose@c1r7s10.42luanda.com>
2024-07-06 18:06:15 +0100

    update ft_word_count(const char *s, char d)

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: xjose <xjose@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 16:28:41 by xjose             #+#    #+#             */
-/*   Updated: 2024/07/05 16:32:50 by xjose            ###   ########.fr       */
+/*   Updated: 2024/07/06 18:05:16 by xjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@ int	main(int argc, char *argv[])
 {
 	char	**result;
 
-	result = ft_split(argv[1], ' ');
-	printf("%s\n", result[0]);
+	result = ft_split(argv[1], '|');
+	while (*result)
+	{
+		printf("%s\n", *result);
+		result++;
+	}
 	return (0);
 }
